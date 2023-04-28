e8add55b13b0bf6eeafb8301dfdb2d7a8668a0c4
Mark Jackson <mark.jackson@red-gate.com>
2023-04-28 13:44:57 +0100

    Tidying

--- CodeKatas/Week5/Command.cs
+++ CodeKatas/Week5/Command.cs
@@ -1,25 +1,7 @@
-﻿using gol;
-using MathNet.Numerics.LinearAlgebra;
-
+﻿
 namespace CodeKatas.Week5;
 
-public abstract record Command
+public abstract record Command(int TurnDirection, int MoveDistance)
 {
-    private readonly int TurnDirection;
-    private readonly int MoveDistance;
-
-    // protected Matrix<double> TranslationMatrix;
-    
-    protected Command(int turnDirection, int moveDistance)
-    {
-        TurnDirection = turnDirection;
-        MoveDistance = moveDistance;
-        // TranslationMatrix = Matrix<double>.Build.DenseOfArray(new double[,]
-        // {
-        //     {MoveDistance},
-        //     {MoveDistance}
-        // });
-    }
-
     public abstract void Execute(MarsRover marsRover);
 }
--- CodeKatas/Week5/TurnCommand.cs
+++ CodeKatas/Week5/TurnCommand.cs
@@ -8,12 +8,6 @@ public record TurnCommand(int TurnDirection): Command(TurnDirection, 0)
     public override void Execute(MarsRover marsRover)
     {
         var angleOfRotationInRadians = TurnDirection * Math.PI / 2;
-        // var newDirection = new Point(
-        //     marsRover.Direction.X * (double)Math.Cos(angleOfRotationInRadians) - marsRover.Direction.Y * (double)Math.Sin(angleOfRotationInRadians),
-        //     marsRover.Direction.X * (double)Math.Sin(angleOfRotationInRadians) + marsRover.Direction.Y * (double)Math.Cos(angleOfRotationInRadians)
-        // );
-        // marsRover.Direction = newDirection;
-        
         var rotationMatrix = Matrix<double>.Build.DenseOfArray(new double[,]
         {
             {Math.Cos(angleOfRotationInRadians), -Math.Sin(angleOfRotationInRadians)},
