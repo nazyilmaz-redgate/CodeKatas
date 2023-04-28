e3b2991083c0668b1070131afe8273069bcf4f29
Mark Jackson <mark.jackson@red-gate.com>
2023-04-28 13:00:30 +0100

    Complete turn tests

--- CodeKatas.Test/Week5/MarsRoverTests.cs
+++ CodeKatas.Test/Week5/MarsRoverTests.cs
@@ -17,15 +17,28 @@ public class MarsRoverTests
         marsRover.Should().BeEquivalentTo(new MarsRover(new Point(0,1), MarsRoverDirections.North, new List<Command>()));
     }
     
-    [Test]
-    public void ExecuteCommand_TurnLeftCommand_Success()
+    [TestCaseSource(nameof(TurnCases))]
+    public void ExecuteCommand_TurnCommand_CorrectEndDirection(int turnDirection, Point beginDirection, Point endDirection)
     {
-        var turnLeftCommands = new List<Command> {new TurnCommand(1)};
-        var marsRover = new MarsRover(new Point(0,0), MarsRoverDirections.North, turnLeftCommands);
+        var commands = new List<Command> {new TurnCommand(turnDirection)};
+        var marsRover = new MarsRover(new Point(0,0), beginDirection, commands);
 
         marsRover.ExecuteCommands();
 
-        marsRover.Should().BeEquivalentTo(new MarsRover(new Point(0,0), MarsRoverDirections.West, new List<Command>()));
+        marsRover.Should().BeEquivalentTo(new MarsRover(new Point(0,0), endDirection, new List<Command>()));
     }
-    
+
+    public static object[] TurnCases =
+    {   //Left
+        new object[] { 1, MarsRoverDirections.North, MarsRoverDirections.West },
+        new object[] { 1, MarsRoverDirections.West, MarsRoverDirections.South },
+        new object[] { 1, MarsRoverDirections.South, MarsRoverDirections.East },
+        new object[] { 1, MarsRoverDirections.East, MarsRoverDirections.North },
+        //Right
+        new object[] { -1, MarsRoverDirections.North, MarsRoverDirections.East },
+        new object[] { -1, MarsRoverDirections.East, MarsRoverDirections.South },
+        new object[] { -1, MarsRoverDirections.South, MarsRoverDirections.West },
+        new object[] { -1, MarsRoverDirections.West, MarsRoverDirections.North }
+    };
+
 }
