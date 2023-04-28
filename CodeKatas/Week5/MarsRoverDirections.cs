f0d89d5e110dc4fa93d61c5d1e7a829e1ebeaee2
Mark Jackson <mark.jackson@red-gate.com>
2023-04-28 13:58:53 +0100

    BuildDirection

--- CodeKatas/Week5/MarsRoverDirections.cs
+++ CodeKatas/Week5/MarsRoverDirections.cs
@@ -5,11 +5,16 @@ namespace CodeKatas.Week5;
 
 public static class MarsRoverDirections
 {
-    public static Matrix<double> North => Matrix<double>.Build.DenseOfArray(new double[,] {  {0},  {1} });
-    public static Matrix<double> East => Matrix<double>.Build.DenseOfArray(new double[,]  {  {1},  {0} });
-    public static Matrix<double> South => Matrix<double>.Build.DenseOfArray(new double[,] {  {0}, {-1} });
-    public static Matrix<double> West => Matrix<double>.Build.DenseOfArray(new double[,]  { {-1},  {0} });
+    public static Matrix<double> North => BuildDirection(0, 1);
+    public static Matrix<double> East => BuildDirection(1, 0);
+    public static Matrix<double> South => BuildDirection(0, -1);
+    public static Matrix<double> West => BuildDirection(-1, 0);
 
     public const int Left = 1;
     public const int Right = -1;
+    
+    private static Matrix<double> BuildDirection(int x, int y)
+    {
+        return Matrix<double>.Build.DenseOfArray(new double[,] { {x}, {y} });
+    }
 }
